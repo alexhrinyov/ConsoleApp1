ff4599248f0254227373b793924f7557ae8707ab
alexhrinyov <hrinyou29@gmail.com>
2022-09-18 15:57:59 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            const string MyName = "Jane";
+
+            Console.WriteLine(MyName);
+
+            Console.WriteLine("Привет, мир");
+            Console.WriteLine("Мне 27 лет");
+            Console.WriteLine("My name is Jane");
+
+            Console.ReadKey();
+        }
+    }
+}
